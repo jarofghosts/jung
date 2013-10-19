9fc2fcda0457fcf7d7d0da257a9c40dc86385bf3
jesse keane <me@jessekeane.me>
2013-10-19 16:15:39 -0700

    move this

---
+++ bin/jung.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var Jung = require('../'),
+    nopt = require('nopt'),
+    noptions = {
+      root: Array,
+      files: Array,
+      dirs: Array,
+      notfiles: Array,
+      notdirs: Array,
+      wait: Number,
+      kill: Boolean,
+      quiet: Boolean,
+      help: Boolean,
+      version: Boolean
+    },
+    shorts = {
+      r: ['--root'],
+      d: ['--dirs'],
+      f: ['--files'],
+      w: ['--wait'],
+      D: ['--notdirs'],
+      F: ['--notfiles'],
+      q: ['--quiet'],
+      k: ['--kill'],
+      h: ['--help'],
+      v: ['--version']
+    },
+  options = nopt(noptions, shorts, process.argv),
+  command = options.argv.remain
+
+if (options.version) return Jung.version()
+if (!command.length || options.help) return Jung.help()
+
+return new Jung(options, command)
