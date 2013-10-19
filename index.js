504f55f7578191605e400406ed2733d2ad9be253
jesse keane <me@jessekeane.me>
2013-10-19 11:47:00 -0700

    cleanup a bit

--- index.js
+++ index.js
@@ -6,37 +6,59 @@ var Watcher = require('fs-watch').Watcher,
     spawn = require('child_process').spawn,
     debounce = require('lodash.debounce'),
     blocked = false,
-    command,
-    watcher
-
-var noptions = {
-  root: Array,
-  files: Array,
-  dirs: Array,
-  notfiles: Array,
-  notdirs: Array,
-  wait: Number,
-  verbose: Boolean,
-  help: Boolean,
-  version: Boolean
-},
-    shorts = {
-  r: ['--root'],
-  d: ['--dirs'],
-  f: ['--files'],
-  w: ['--wait'],
-  D: ['--notdirs'],
-  F: ['--notfiles'],
-  v: ['--verbose'],
-  h: ['--help'],
-  V: ['--version']
-},
-options = nopt(noptions, shorts, process.argv)
-command = options.argv.remain
+    noptions = {
+      root: Array,
+      files: Array,
+      dirs: Array,
+      notfiles: Array,
+      notdirs: Array,
+      wait: Number,
+      verbose: Boolean,
+      help: Boolean,
+      version: Boolean
+    },
+        shorts = {
+      r: ['--root'],
+      d: ['--dirs'],
+      f: ['--files'],
+      w: ['--wait'],
+      D: ['--notdirs'],
+      F: ['--notfiles'],
+      v: ['--verbose'],
+      h: ['--help'],
+      V: ['--version']
+    },
+  options = nopt(noptions, shorts, process.argv),
+  command = options.argv.remain
 
 if (!command.length || options.help) return help()
 if (!options.root || !options.root.length) options.root = [process.cwd()]
 if (!options.wait) options.wait = 1000
+
+var watcher_options = { paths: options.root, filters: {} },
+    watcher = new Watcher(watcher_options)
+
+watcher.on('any', debounce(trigger_command, options.wait))
+
+watcher.start()
+
+function trigger_command() {
+  if (blocked) return console.log('previous process still running')
+  blocked = true
+  var child = spawn(command[0], command.slice(1))
+  child.on('close', finish_child)
+
+  if (options.verbose) {
+    child.process.stdout.pipe(process.stdout)
+    child.process.stderr.pipe(process.stderr)
+  }
+
+  function finish_child(code) {
+    if (code !== 0) process.stderr.write('command exited with code ' + code)
+    blocked = false
+  }
+
+}
 
 function make_filter(type) {
   var not_array = type === 'file' ? options.notfiles : options.notdirs,
@@ -59,31 +81,6 @@ function make_filter(type) {
   }
 }
 
-function dir_filter() {
+function help() {
+  console.log('no')
 }
-
-var watcher_options = { paths: options.root, filters: {} }
-
-watcher = new Watcher(watcher_options)
-
-watcher.on('any', debounce(trigger_command, options.wait))
-
-function trigger_command() {
-  if (blocked) return console.log('previous process still running')
-  blocked = true
-  var child = spawn(command[0], command.slice(1))
-  child.on('close', finish_child)
-
-  if (options.verbose) {
-    child.process.stdout.pipe(process.stdout)
-    child.process.stderr.pipe(process.stderr)
-  }
-
-  function finish_child(code) {
-    if (code !== 0) process.stderr.write('command exited with code ' + code)
-    blocked = false
-  }
-
-}
-
-watcher.start()
